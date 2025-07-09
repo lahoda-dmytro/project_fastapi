54d01b1fc665996e2044d748710e4eafcbb7f188
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-07-09 21:01:37 +0300

    implement class "User", modified post`s fields, added authors

--- main.py
+++ main.py
@@ -4,25 +4,33 @@ from pydantic import BaseModel
 app = FastAPI()
 
 
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
+
 
 class Post(BaseModel):
     id: int
     title: str
     body: str
+    author: User
 
+users = [
+    {'id': 1, 'username': 'user1', 'age': 1},
+    {'id': 2, 'username': 'user2', 'age': 2},
+    {'id': 3, 'username': 'user3', 'age': 3},
+]
 
 posts = [
-    {'id': 1, 'title': 'news_1', 'body': 'text_1'},
-    {'id': 2, 'title': 'news_2', 'body': 'text_2'},
-    {'id': 3, 'title': 'news_3', 'body': 'text_3'}
+    {'id': 1, 'title': 'news_1', 'body': 'text_1', 'author': users[0]},
+    {'id': 2, 'title': 'news_2', 'body': 'text_2', 'author': users[1]},
+    {'id': 3, 'title': 'news_3', 'body': 'text_3', 'author': users[2]},
 ]
 
 @app.get("/items")
 async def items() -> List[Post]:
-    post_list = []
-    for post in posts:
-        post_list.append(Post(id=post['id'], title=post['title'], body=post['body']))
-    return post_list
+    return [Post(**post) for post in posts]
 
 @app.get("/item/{id}")
 async def item(id: int) -> Post:
