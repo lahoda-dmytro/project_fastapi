521a37630c16b0b206c1064ed7e069a7b0dd0228
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-07-10 19:20:01 +0300

    created schemas.py

---
+++ schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+    age: int
+
+class UserCreate(UserBase):
+    pass
+
+
+class User(UserBase):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
+class PostBase(BaseModel):
+    title: str
+    body: str
+    author_id: int
+
+
+class PostCreate(PostBase):
+    pass
+
+
+class Post(PostBase):
+    id: int
+
+    class Config:
+        orm_mode = True
