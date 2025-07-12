a05504c93a3aac565fd00b7e2f266f0279090f3c
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-07-12 17:12:26 +0300

    fixed get("/posts/) method

--- main.py
+++ main.py
@@ -44,42 +44,10 @@ async def create_post(post: PostCreate, db: Session = Depends(get_db)) -> DbPost
 
 @app.get("/posts/", response_model=List[DbPost])
 async def get_posts(db: Session = Depends(get_db)):
-    return db.query(DbPost).all()
+    return db.query(Post).all()
 
 
 
-# @app.get("/items")
-# async def items() -> List[Post]:
-#     return [Post(**post) for post in posts]
-#
-# @app.post("/items/add")
-# async def add_item(post: PostCreate) -> Post:
-#     author = next((user for user in users if user['id'] == post.author_id), None)
-#     if not author:
-#         raise HTTPException(status_code=404, detail="user not found")
-#
-#     new_post_id = max((p['id'] for p in posts), default=0) + 1
-#
-#     new_post = {'id': new_post_id,
-#         'title': post.title,
-#         'body': post.body,
-#         'author': author}
-#
-#     posts.append(new_post)
-#
-#     return Post(**new_post)
-#
-# @app.post("/user/add")
-# async def user_add(user: UserCreate) -> User:
-#     new_user_id = max((user_id['id'] for user_id in users), default=0) + 1
-#     new_user = {'id': new_user_id,
-#         'username': user.username,
-#         'age': user.age }
-#
-#     posts.append(new_user)
-#
-#     return User(**new_user)
-#
 # @app.delete("/items/delete/{id}")
 # async def delete_item(id: Annotated[int, Path(..., title="post`s id", ge=1)]) -> Post:
 #    for index, post in enumerate(posts):
