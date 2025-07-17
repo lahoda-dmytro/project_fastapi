222625ce3cf1f82d32cc97e42ef43b0693eb7033
Лагода Дмитро Олександрович <ipz235_ldo@student.ztu.edu.ua>
2025-07-17 19:39:08 +0300

    connected to postgresql

--- database.py
+++ database.py
@@ -1,10 +1,14 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
-SQL_DB_URL = 'sqlite:///./fastapiproject.db'
+load_dotenv()
 
-engine = create_engine(SQL_DB_URL, connect_args={"check_same_thread": False})
+SQL_DB_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(SQL_DB_URL, connect_args={"check_same_thread": False} if "sqlite" in SQL_DB_URL else {})
 
 session_local = sessionmaker(autoflush=False, autocommit=False, bind=engine)
 
